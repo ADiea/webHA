5446ed989d5de7d16833c9014bc64c5f66f644a6
unknown <diea_andrei@yahoo.com>
2016-02-14 22:50:40 +0200

    db

--- web/dbadmin/classes/database/Connection.php
+++ web/dbadmin/classes/database/Connection.php
@@ -87,6 +87,8 @@ class Connection {
 			case '7.5': return 'Postgres80'; break;
 			case '7.4': return 'Postgres74'; break;
 			case '9.2': return 'Postgres92'; break;
+			case '9.4': return 'Postgres92'; break;
+			case '9.4.4': return 'Postgres92'; break;
 		}
 
 		/* All <7.4 versions are not supported */

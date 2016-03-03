a5bdf75be9e0149d4338f4ca5189b3f3b5ce63d2
unknown <diea_andrei@yahoo.com>
2016-03-03 23:39:19 +0200

    switch to nodejs :)

---
+++ index.js
@@ -0,0 +1,38 @@
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
+
+app.use(express.static('www'));
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+// API Routes
+// app.get('/blah', routeHandler);
+
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})
--- web/js/controllers/FMController.js
+++
@@ -1,7 +0,0 @@
-var _FMCtrl = ionicApp.controller('FMRadioCtrl', function($scope, Lang) 
-{
-	$scope.lang = Lang;
-	$scope.radioTitle = "Bathroom Radio";
-	
-	$scope.radioOn = true;
-});

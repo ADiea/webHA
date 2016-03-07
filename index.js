8da4f0fd2d83b29b85f9cf65ca5c842b77fb3c1c
unknown <diea_andrei@yahoo.com>
2016-03-07 08:19:55 +0200

    start ws srv

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
 
-app.use(express.static(__dirname + '/temp'));
+app.use(express.static(__dirname + '/site'));
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
@@ -25,15 +25,103 @@ console.log("http server listening on %d", port)
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
+//connections
+var gCon = {};
+
+var curID = 0;
+
+var WsProtocol = 
+{
+	//operation code 
+	wsOP_cliHello:0,
+	wsOP_servHello:1,
+	wsOP_cliLogin:2,
+	wsOP_msgToBase:3,
+	wsOP_msgToUser:4,
+	
+	//message keys
+	wsKey_clientType:0,
+	
+	//message values
+	wsValue_homeBase:0,
+	wsValue_webBrowser:1,
+	wsValue_mobileApp:2,
+	
+	//connection states
+	wsState_new:0,
+	wsState_hello:1,
+	wsState_conn:2,
+	wsState_inval:3,
+	
+};
+
+function getConIdx(id)
+{
+	var i;
+	for(i=0; i< gCon.length; i++)
+	{
+		if(id == gCon[i].id)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+wss.on("connection", function(ws) 
+{
+  var id = curID++;
+  var conIdx = gCon.length;
+  
+  gCon.push({id:id, ws:ws, state:WsProtocol.wsState_new});
+  
+  /*setInterval(function() {
     ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+  }, 1000)*/
 
-  console.log("websocket connection open")
+  //ws.send("{conID:"+id+"}", function() {  })
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+  console.log("ws open "+id);
+   
+  ws.on("message", function(msg) 
+  {
+    console.log("ws msg "+msg)
+    
+	var m = JSON.???(msg);
+	
+	switch(m.op)
+	{
+		case WsProtocol.wsOP_cliHello:
+			if(gCon[conIdx].state == WsProtocol.wsState_new)
+			{
+				ws.send("{conID:"+id+"}", function() {  })
+			
+				gCon[conIdx].state = WsProtocol.wsState_hello;
+			}
+			else console.log("[E] ws msg: cliHello, state "+gCon[conIdx].state);
+		break;
+		
+		case WsProtocol.wsOP_cliLogin:
+			if(gCon[conIdx].state == WsProtocol.wsState_new)
+			{
+				gCon[conIdx].state = WsProtocol.wsState_hello;
+			}
+			else console.log("[E] ws msg: cliLogin, state "+gCon[conIdx].state);		
+		break;
+		
+		case WsProtocol.wsOP_msgToBase:
+		break;
+		
+		case WsProtocol.wsOP_msgToUser:
+		break;
+	};
+	
+  });
+
+  ws.on("close", function() 
+  {
+    console.log("ws close "+id)
+    //clearInterval(id);
+  });
+});
